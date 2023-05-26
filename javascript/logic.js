5ab93f92f8ce833148b1379b70e3b97b9f1be55d
ShaviyaVictor <shaviyavictor@gmail.com>
2023-05-26 15:32:48 -0500

    Use the repeat() method to repeat a String a number of times

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,12 +1,31 @@
 
+// 26052023
 // Repeating a String 
 //  -> Longhand 
 //  -- Using a Loop to repeat a String multiple times 
+  // initialize an empty String 
+  // for loop that increases by index until it gets to the defined number
+    // equitting the initialized empty string to take the defined String incrementally
+// calling the function 
+
 function repeatString(String, number) {
   let results = '';
-  for (let i=0; i<number; i++) {
-    results += String
+  for (i=0; i<number; i++) {
+    results += String;
   }
   return results
 }
-console.log(repeatString('Shaviya', 5));
+console.log(repeatString('Shaviya', 5))
+
+//  -> Shorthand 
+// -- Using the repeat() method
+
+let myName = 'Shaviya';
+console.log(myName.repeat(5))
+
+// or 
+
+console.log('Shaviya'.repeat(5))
+
+
+
