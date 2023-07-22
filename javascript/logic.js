67c7aa5458558080db39c3b47b279879a2e800ea
ShaviyaVictor <shaviyavictor@gmail.com>
2023-07-22 11:25:17 -0500

    Create a function that simulates an implementation of a bubble sort algorithm

--- javascript/logic.js
+++ javascript/logic.js
@@ -10,6 +10,19 @@
 
   // ? =======> Bubble Sort
 
+  function bubbleSort(array) {
+    for (let i=0; i<array.length; i++) {
+      for (let j=0; j<array.length -i -1; j++) {
+        if (array[j] > array[j + 1]) {
+          const temporaryValue = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = temporaryValue;
+        }
+      }
+    }
+    return array;
+  };
+
 // #####    22072023    #####
 
 
