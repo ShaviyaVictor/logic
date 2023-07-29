6daab73829f036c9d0fae64980187509d9addaed
ShaviyaVictor <shaviyavictor@gmail.com>
2023-07-29 12:04:37 -0500

    Add the logic checks with the loop flows

--- javascript/logic.js
+++ javascript/logic.js
@@ -19,6 +19,19 @@ console.log(searchArray);
     let left = 0;
     let right = searchArray.length - 1;
 
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+
+      if (searchArray[mid] === target) {
+        return mid;
+      } else if (searchArray[mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid -1;
+      }
+    }
+    return -1;
+
   }
 
 // #####    29072023    #####
