a966009d3e28ffde4fd1afd9e1a87f5cefa22dd2
ShaviyaVictor <shaviyavictor@gmail.com>
2023-08-02 10:58:38 -0500

    Create the array to be searched by breadth-first algo

--- javascript/logic.js
+++ javascript/logic.js
@@ -58,6 +58,28 @@ console.log(searchArray);
 
   }
 
+  // Example
+  const tree = {
+    value: 1,
+    children: [
+      {
+        value: 2,
+        children: [
+          { value: 4, children: [] },
+          { value: 5, children: [] }
+        ]
+      },
+      {
+        value: 3,
+        children: [
+          { value: 6, children: [] },
+          { value: 7, children: [] }
+        ]
+      },
+    ]
+  };
+  let breadthTarget = 5;
+
 // #####    30072023    #####
 
 // #####  Day42:  29072023    #####
